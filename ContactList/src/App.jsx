1ace096d9c01943f070dd4ece1ba0a811a406117
Christina <christinah.0821@gmail.com>
2024-04-03 21:54:39 -0700

    in progress

--- ContactList/src/App.jsx
+++ ContactList/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import ContactList from "./components/ContactList";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <ContactList />
       <h1>Contact List</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
